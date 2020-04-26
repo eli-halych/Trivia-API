8e2b8a61f493d86fc39c490cfbedd6beb015156d
Illia Halych <middlezwei@gmail.com>
2020-04-26 21:22:25 +0200

    Cross Origin Resource Sharing (CORS)
    - Allow response headers after requests

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -13,10 +13,14 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
-    '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+    # CORS
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
 
     '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
